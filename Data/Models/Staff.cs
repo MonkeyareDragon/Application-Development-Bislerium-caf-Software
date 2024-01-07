6b7d454b6e1746ae61634dcabd8806ec8bc5dcab
MonkeyareDragon <githubforme73@gmail.com>
2024-01-07 13:59:53 +0545

    Login is full completed.

--- Data/Models/Staff.cs
+++ Data/Models/Staff.cs
@@ -1,27 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BisleriumCafé.Data.Models;
 
-namespace BisleriumCafé.Data.Models
+namespace BisleriumCafé.Data.Models;
+
+public class StaffModel
 {
-    internal class Staff
-    {
-        public class StaffModel
-        {
-            // Attributes
-            public string Name { get; set; }
-            private string Password { get; set; }
-            public string Role { get; set; }
-
-            // Constructor
-            public StaffModel(string name, string password, string role)
-            {
-                Name = name;
-                Password = password;
-                Role = role;
-            }
-        }
-    }
+    // Attributes
+    public string Name { get; set; }
+    public StaffPassword Password { get; set; }
+    public StaffRole Role { get; set; }
 }
